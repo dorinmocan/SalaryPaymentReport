ea9bc5f81adf54219bc8a7595bd08b2e257953c9
Mocan Dorin <corpze_fertiliser@mail.ru>
2018-10-12 14:16:52 +0300

    Employee and SalaryPayment classes added

---
+++ SalaryPaymentReport/SalaryPaymentReport/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryPaymentReport
+{
+    internal class Employee
+    {
+        internal Employee(uint id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        internal readonly uint Id;
+        internal readonly string FirstName;
+        internal readonly string LastName;
+    }
+}
