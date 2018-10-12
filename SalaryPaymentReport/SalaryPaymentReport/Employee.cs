5a5bf28a7e574a8a4c05d2877517149ba82d49af
Mocan Dorin <corpze_fertiliser@mail.ru>
2018-10-12 17:33:22 +0300

    Added validation for Employee class

--- SalaryPaymentReport/SalaryPaymentReport/Employee.cs
+++ SalaryPaymentReport/SalaryPaymentReport/Employee.cs
@@ -10,13 +10,44 @@ namespace SalaryPaymentReport
     {
         internal Employee(uint id, string firstName, string lastName)
         {
+            if (!IsValidId(id)) throw new ArgumentException("Invalid value for parameter " + @id);
+            if (!IsValidFirstName(firstName)) throw new ArgumentException("Invalid value for parameter " + @firstName);
+            if (!IsValidLastName(lastName)) throw new ArgumentException("Invalid value for parameter " + @lastName);
+
             Id = id;
             FirstName = firstName;
             LastName = lastName;
         }
 
-        internal readonly uint Id;
-        internal readonly string FirstName;
-        internal readonly string LastName;
+        internal bool IsValidId(uint id)
+        {
+            if (id == 0) return false;
+
+            return true;
+        }
+
+        private bool IsValidFirstName(string firstName)
+        {
+            return IsValidName(firstName);
+        }
+
+        private bool IsValidLastName(string lastName)
+        {
+            return IsValidName(lastName);
+        }
+
+        private bool IsValidName(string name)
+        {
+            foreach (char letter in name)
+            {
+                if (!char.IsLetter(letter)) return false;
+            }
+
+            return true;
+        }
+
+        internal uint Id { get; }
+        internal string FirstName { get; }
+        internal string LastName { get; }
     }
 }
